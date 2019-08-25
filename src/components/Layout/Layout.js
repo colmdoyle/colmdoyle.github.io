fcedb2194a9aca829e6d8ec4ef314fc8241d50c5
Aleksander Helgaker <aleksander.helgaker@protonmail.com>
2019-08-25 11:14:14 +0200

    Add the twitter:description meta tag.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -19,6 +19,7 @@ const Layout = ({ children, title, description }: Props) => (
       <meta property="og:site_name" content={title} />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
     </Helmet>
     {children}
   </div>
