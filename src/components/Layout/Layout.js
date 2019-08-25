849ef16d679c5e8608dd6f2d0c1772615d51324c
Aleksander Helgaker <aleksander.helgaker@protonmail.com>
2019-08-25 14:50:58 +0200

    Fix QA comment.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -30,6 +30,7 @@ const Layout = ({
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta property="og:site_name" content={title} />
+        <meta property="og:image" content={metaImageUrl} />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
