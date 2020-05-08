66ee71d4cfe744987a68ff8e3072a192ffdabe6a
Samuel W <sammatwong@gmail.com>
2020-05-09 00:12:57 +0800

    Fix Print Width

--- config.js
+++ config.js
@@ -4,8 +4,7 @@ module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
   title: 'Blog by John Doe',
-  subtitle:
-    'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -28,8 +27,7 @@ module.exports = {
   author: {
     name: 'John Doe',
     photo: '/photo.jpg',
-    bio:
-      'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
       email: '',
       facebook: '#',
--- src/utils/get-contact-href.test.js
+++ src/utils/get-contact-href.test.js
@@ -15,9 +15,7 @@ test('getContactHref', () => {
   expect(getContactHref('gitlab', '#')).toBe('https://www.gitlab.com/#');
   expect(getContactHref('weibo', '#')).toBe('https://www.weibo.com/#');
   expect(getContactHref('codepen', '#')).toBe('https://www.codepen.io/#');
-  expect(getContactHref('youtube', '#')).toBe(
-    'https://www.youtube.com/channel/#'
-  );
+  expect(getContactHref('youtube', '#')).toBe('https://www.youtube.com/channel/#');
   expect(getContactHref('soundcloud', '#')).toBe('https://soundcloud.com/#');
   expect(getContactHref('medium', '#')).toBe('https://medium.com/#');
 });
