c51833b50186aaf78c84b4adf564e6d761a2a7b5
Colm Doyle <colm@cdoyle.me>
2021-05-12 09:39:44 +0100

    redirects

--- gatsby/create-pages.js
+++ gatsby/create-pages.js
@@ -7,7 +7,7 @@ const createTagsPages = require('./pagination/create-tags-pages.js');
 const createPostsPages = require('./pagination/create-posts-pages.js');
 
 const createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
+  const { createPage, createRedirect } = actions;
 
   // 404
   createPage({
@@ -65,10 +65,22 @@ const createPages = async ({ graphql, actions }) => {
     }
   });
 
+  
   // Feeds
   await createTagsPages(graphql, actions);
   await createCategoriesPages(graphql, actions);
   await createPostsPages(graphql, actions);
+  
+  createRedirect({ fromPath: '/posts/when-you-struggle-to-write', toPath: '/posts/2021/05/when-you-struggle-to-write', isPermanent: true });
+  createRedirect({ fromPath: '/posts/building-for-the-new-normal', toPath: '/posts/2021/04/building-for-the-new-normal', isPermanent: true });
+  createRedirect({ fromPath: '/posts/introducing-klokta', toPath: '/posts/2021/04/introducing-klokta', isPermanent: true });
+  createRedirect({ fromPath: '/posts/spring-loaded-scorecard', toPath: '/posts/2021/04/spring-loaded-scorecard', isPermanent: true });
+  createRedirect({ fromPath: '/posts/predictions-apple-spring-loaded', toPath: '/posts/2021/04/predictions-apple-spring-loaded', isPermanent: true });
+  createRedirect({ fromPath: '/posts/if-youre-not-iterating-youre-falling-behind', toPath: '/posts/2021/04/if-youre-not-iterating-youre-falling-behind', isPermanent: true });
+  createRedirect({ fromPath: '/posts/getting-better-at-devrel', toPath: '/posts/2021/04/getting-better-at-devrel', isPermanent: true });
+  createRedirect({ fromPath: '/posts/the-real-perk', toPath: '/posts/2016/05/the-real-perk', isPermanent: true });
+  createRedirect({ fromPath: '/posts/families-and-tech-companies', toPath: '/posts/2016/04/families-and-tech-companies', isPermanent: true });
+  createRedirect({ fromPath: '/posts/it-s-time-to-get-rid-of-traditional-release-notes', toPath: '/posts/2016/02/it-s-time-to-get-rid-of-traditional-release-notes', isPermanent: true });
 };
 
 module.exports = createPages;
