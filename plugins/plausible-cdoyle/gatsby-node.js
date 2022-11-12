3c1d80097ec3f3d6e3a01f47fae011ff90ecfda1
Colm Doyle <colmdoyle@users.noreply.github.com>
2022-11-12 12:23:12 +0000

    nuke

--- plugins/plausible-cdoyle/gatsby-node.js
+++
@@ -1,7 +0,0 @@
-exports.onPreInit = ({ reporter }, options) => {
-  if (!options.domain) {
-    reporter.warn(
-      `The Plausible Analytics plugin requires a domain. Did you mean to add it?`
-    );
-  }
-};
